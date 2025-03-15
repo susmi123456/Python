c664d3edbe6af1eb1c2f71712a5ea8941817defc
susmi123456 <susmithagunapati630@gmail.com>
2025-03-15 12:30:18 +0530

    ok

--- Class-8 iterative/six.py
+++ Class-8 iterative/six.py
@@ -4,15 +4,39 @@
 
 #using for loop while loop 
 
+#using for loop
+enames=["Rahul","Priyaka","Sonia","Modi"]
+for i in enames:
+    print(i) 
 
-# enames=["RG","PG","SG","Modi"]
-# for i in enames:
-#     print(i)
+unames=("Rahul","Priyaka","Sonia","Modi")
+for i in unames:
+    print(i)
 
-# unames=("RG","PG","SG","Modi")
-# for i in unames:
-#     print(i)
-
-accnames=("RG","PG","SG","Modi")
+accnames={"Rahul","Priyaka","Sonia","Modi"}
 for i in accnames:
     print(i)
+
+#using while loop
+enames=["Rahul","Priyaka","Sonia","Modi"]
+# index  0     1    2    3    
+i=0
+while i==0:
+    print(enames)    #['Rahul', 'Priyaka', 'Sonia', 'Modi']
+    i=i+1      
+
+enames=("Rahul","Priyaka","Sonia","Modi")
+# index  0     1    2    3    
+i=0
+while i==0:
+    print(enames)   #("Rahul","Priyaka","Sonia","Modi")
+    i=i+1
+
+
+enames={"Rahul","Priyaka","Sonia","Modi"}
+# index  0     1    2    3    
+i=0
+while i==0: 
+    print(enames)   #{"Rahul","Priyaka","Sonia","Modi"}
+    i=i+1
+
